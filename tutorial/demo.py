7cf099922601e8ebebfa247a6884aad74a11ce8c
cgehrerc <46731953+cgehrerc@users.noreply.github.com>
2019-05-05 18:35:38 -0400

    added grids for layout

--- tutorial/demo.py
+++ tutorial/demo.py
@@ -14,24 +14,30 @@ class App:
          self.window = window
          self.window.title(window_title)
          self.video_source = video_source
-
-
-
+         self.pause = False
          #open video source
          self.vid = MyVideoCapture(video_source)
 
          #create canvas
-         self.canvas = tkinter.Canvas(window, width = self.vid.width, height=  self.vid.height)
-         self.canvas.pack()
+         self.canvas = tkinter.Canvas(window, width = self.vid.width/1.25, height=self.vid.height/1.25)
+         self.canvas.grid(column=0, row=0, columnspan=3, rowspan=2)
 
-         
+         #button that lets user pause the video
+         self.btn_pause=tkinter.Button(window, text="Pause", width=15, command=self.pause)
+         self.btn_pause.grid(column=0, row=3)
          # Button that lets the user take a snapshot
-         self.btn_snapshot=tkinter.Button(window, text="Snapshot", width=50, command=self.snapshot)
-         self.btn_snapshot.pack(anchor=tkinter.CENTER, expand=True)
-
+         self.btn_snapshot=tkinter.Button(window, text="Snapshot", width=15, command=self.snapshot)
+         self.btn_snapshot.grid(column=1, row=3)
+         #where we enter the information to complete the algorithm
+         self.detail=tkinter.Frame(window,borderwidth=5, relief="sunken", width=500, height=300)
+         self.detail.grid(column=4, row=0)
+         #where we display the list of threats for a given mission
+         self.threats=tkinter.Frame(window, width=500, height=300)
+         self.threats.grid(column=4, row=3)
          #auto call until stopped
          self.delay = 15
          self.update()
+
 
          self.window.mainloop()
 
@@ -40,8 +46,10 @@ class App:
       ret, frame = self.vid.get_frame()
 
       if ret:
-         self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
-         self.canvas.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
+         if self.pause == False:
+            
+            self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
 
       self.window.after(self.delay, self.update)
 
@@ -50,6 +58,8 @@ class App:
      ret, frame = self.vid.get_frame()
      if ret:
         cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+   def pause(self):
+      self.pause = true
 class MyVideoCapture:
     def __init__(self, video_source=0):
       #open the video source
