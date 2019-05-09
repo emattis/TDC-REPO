4183f62ce7cb11ced3e7973b4bbc3a92ba7acd97
djaureguitor <djaureguitor@gmail.com>
2019-05-08 20:45:20 -0400

    contour

--- tutorial/demo.py
+++ tutorial/demo.py
@@ -110,6 +110,44 @@ class MyVideoCapture:
 
         # compute the bounding box of the of the paper region and return it
         return cv2.minAreaRect(c)
+
+    def distance_to_camera(knownWidth, focalLength, perWidth):
+        # compute and return the distance from the maker to the camera
+        return (knownWidth * focalLength) / perWidth
+
+    # initialize the known distance from the camera to the object, which
+    # in this case is 24 inches
+    KNOWN_DISTANCE = 24.0
+
+    # initialize the known object width, which in this case, the piece of
+    # paper is 12 inches wide
+    KNOWN_WIDTH = 11.0
+
+    # load the furst image that contains an object that is KNOWN TO BE 2 feet
+    # from our camera, then find the paper marker in the image, and initialize
+    # the focal length
+    image = cv2.imread("images/2ft.png")
+    marker = find_marker(image)
+    focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
+
+    # loop over the images
+    for imagePath in sorted(paths.list_images("images")):
+        # load the image, find the marker in the image, then compute the
+        # distance to the marker from the camera
+        image = cv2.imread(imagePath)
+        marker = find_marker(image)
+        inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
+
+        # draw a bounding box around the image and display it
+        box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
+        box = np.int0(box)
+        cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
+        cv2.putText(image, "%.2fft" % (inches / 12),
+                    (image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
+                    2.0, (0, 255, 0), 3)
+        cv2.imshow("image", image)
+        cv2.waitKey(0)
+        
  # Create a window and pass it to the Application object
  #this is where we call the video file
 App(tkinter.Tk(), "R2D2", "puppy.mov")
